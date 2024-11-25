44ea88c574356805a87e629b38d5a721b04c3247
Omar Gamal <agamyo168@gmail.com>
2024-11-26 01:23:23 +0200

    add order controller

--- src/controllers/order-controller.ts
+++ src/controllers/order-controller.ts
@@ -1,6 +1,18 @@
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 
+export const create = async (_req: Request, res: Response) => {
+  res
+    .status(StatusCodes.CREATED)
+    .json({ success: true, result: 'Created a new order' });
+};
 export const getOrder = async (_req: Request, res: Response) => {
-  res.status(StatusCodes.OK).json({ success: true, result: 'Order retrieved' });
+  res
+    .status(StatusCodes.OK)
+    .json({ success: true, result: 'Retrieved all orders' });
+};
+export const getOrderById = async (_req: Request, res: Response) => {
+  res
+    .status(StatusCodes.OK)
+    .json({ success: true, result: 'Retrieved a single order' });
 };
