2033c9ae5025b4099b3c94ade3f7c6a3bc5286c0
Omar Gamal <agamyo168@gmail.com>
2024-11-26 13:14:01 +0200

    change verifyUser to cerifyCredentialsAndCreateToken & rename Authentication to AuthenticationServer

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import Users, { User } from '../models/user';
 import BadRequestError from '../errors/bad-request-error';
-import Authentication from '../services/authentication-service';
+import AuthenticationService from '../services/authentication-service';
 
 export const getIndex = async (_req: Request, res: Response) => {
   const allUsers: User[] = await Users.findAll();
@@ -39,7 +39,7 @@ export const createUser = async (
   }
   //Need way too many validations -> not_empty. [3, 20] chars.
   //Unique user:
-  const isAvailable = await Authentication.isAvailable(username);
+  const isAvailable = await AuthenticationService.isAvailable(username);
   if (!isAvailable) {
     return next(
       new BadRequestError('Bad request: this username already exists')
