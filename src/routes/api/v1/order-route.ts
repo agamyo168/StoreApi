790ac0921f2f672151afb68a93dfca3a53af54e6
Omar Gamal <agamyo168@gmail.com>
2024-11-27 09:43:27 +0200

    delete code from order controller and route

--- src/routes/api/v1/order-route.ts
+++ src/routes/api/v1/order-route.ts
@@ -1,13 +1,5 @@
 import { Router } from 'express';
-import {
-  getCompletedOrders,
-  getUserOrder,
-} from '../../../controllers/order-controller';
-import authHandler from '../../../middlewares/auth-handler';
 
 const orderRoute = Router();
-orderRoute.use(authHandler);
-orderRoute.route('/order').get(getUserOrder);
-orderRoute.route('/order/product/:id').post();
-orderRoute.route('/order/complete').get(getCompletedOrders);
+
 export default orderRoute;
